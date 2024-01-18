97dcda4aacbfb677aa46dceaead3d63a7a2a1a54
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-01-18 20:40:41 +0500

    Homework is start!

---
+++ script.js
@@ -0,0 +1,6 @@
+const name = document.getElementById('name');
+const inputEmail = document.getElementById('inputEmail');
+const password = document.getElementById('password');
+const address = document.getElementById('addres');
+const city = document.getElementById('city');
+
